db54c3706040cec7aabdfc776843197319e0e450
philippedehovre <ph.dehovre@gmail.com>
2024-04-17 18:05:23 +0100

    16th April dependency update

--- src/Components/Feature2.js
+++ src/Components/Feature2.js
@@ -48,33 +48,22 @@ function Feature2(props) {
                             onClick={() => { setHovering(!hovering) }}
                         >
                             {hovering && <div className='bio'>
-                                <span className='p white'>
-                                    Entrepreneur, professional violinist, artist manager, A&R and concert promoter, with previous experience in executive roles.
-                                </span>
-                                <br />
-                                <span>
-                                    Graduated in Business Management and Music Performance, Marta holds an MBA and a masters in Music Industry Management & Artist Development with Distinction at the University of West London.
-                                </span>
-                                <br />
-                                She gained valuable experience working as Assistant Manager for a
-                                <p>
-                                    <span className='p white'>
-                                        concert promoter and record label in Spain, after which she decided to continue developing her career in the UK
-                                    </span>
-                                    <span>
-                                        {` `}with roles in music management, live events, A&R, social media, marketing and business development.
-                                    </span>
-                                </p>
-                                <br />
-                                <p>
-                                    She’s worked with dozens of artists, including{` `}
-                                    <span className='p white'>
-                                        Billy Lockett, Elder Island, Sam Branson / Waves Rush In, RJ Thompson, Paco Montalvo{` `}
-                                    </span>
-                                    (Spain) and
-                                </p>
-                                hundreds of up and coming artists.
-                                <br />
+                            Entrepreneurial at heart, Marta's whole life has been dedicated to music in some shape or form. 
+
+
+Originally from Spain, she started studying music at the age of 5, choosing violin as her main instrument at the age of 7. After over a decade of building her career as a professional violinist, Marta’s entrepreneurial instinct and passion for business development kicked in, leading her to start her first business at the age of 18.
+
+
+With degrees in Business Management and Music Performance, an MBA and a master's in Music Industry Management and Artist Development from the University of West London, Marta gained valuable experience working as an Assistant Manager for a Spanish concert promoter and management company, after which she decided to continue developing her career in the UK, with roles in artist management, live promotion, A&R, marketing, business operations, and business development.
+
+
+In April 2022, having previous experience in executive roles and driven by her passion for working with artists, and the belief in a better way of doing things in the industry that's fairer for everyone involved, she founded Bedmar Music, a non-traditional artist management company where honesty, transparency, and fairness form the bedrock of every decision.
+
+
+Before starting her own roster, Marta had already worked with dozens of artists, including Billy Lockett, Elder Island, Paco Montalvo (Spain) and numerous up-and-coming artists.
+
+
+With any new project she embarks on, Marta strives to continue redefining industry norms by developing innovative strategies that prioritize artists' well-being and foster a collaborative and supportive environment, where business practices are ethical and success is measured not just by commercial gains but by the genuine fulfilment and growth of each artist and team member under her guidance.
                                 <div style={{ padding: '1em 0 0 0' }}>
                                     <Socials
                                         color={tertiary}
