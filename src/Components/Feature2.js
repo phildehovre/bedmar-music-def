a3af1c5894f5c93e36d85fb40d1f33c0c04d751d
philippedehovre <ph.dehovre@gmail.com>
2024-04-28 12:40:19 +0100

    Fix bio

--- src/Components/Feature2.js
+++ src/Components/Feature2.js
@@ -2,26 +2,14 @@ import React, { useState} from 'react'
 import Section from './Section'
 import Socials from './Socials'
 import './Feature2.css'
-import { bio } from '../Assets/biography'
-// import { animate, inView, timeline, stagger} from 'motion'
 
 function Feature2(props) {
 
-    // useEffect(() => {
-    //     inView('.feature2-ctn', ({target}) => {console.log('in view')
-    //     timeline([
-    //         [
-
-    //             animate('#slide-ctn', {x: [100, 0]}, {duration: 1.5, easing: 'ease-out', })
-    //         ],
-    //         [
-
-    //             animate('#slide-title', {x: [-100, 0]}, {duration: 1.5, easing: 'ease-out', delay: stagger(1)})
-    //         ]
-    //     ])
-    // })
-    // }, []);
-
+    const bio = `Originally from Spain and based in London for the past 5 years, Marta started her career as a professional violinist but quickly fell in love with the business side of things, which led her to start her first business at the age of 18. 
+    Graduated in Business Management and Music Performance, Marta holds an MBA and a masters in Music Industry Management & Artist Development with Distinction at the University of West London.    
+    She gained valuable experience working for a concert promoter and record label in Spain, after which she decided to continue developing her career in the UK with roles in music management, live events, A&R, social media, marketing and business development.    
+    After four years of experience in artist management, Marta founded Bedmar Music in 2022 with a mission to empower and support artists with unique and out-of-the-box strategies, and where honesty, transparency, and fairness form the bedrock of every decision.`
+    
     const { themeColor, text, content, highlight, featureTitle, bgImg, isSmallScreen, tertiary } = props
 
     const [hovering, setHovering] = useState(false)
