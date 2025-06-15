55ab1ca2bcfffb43f1b1f72588f1724ccab9e086
Philippe De Hovre <phil@Philippes-Mac-mini.local>
2025-06-15 13:49:58 +0200

    update artist list

--- src/Assets/ArtistsList.js
+++ src/Assets/ArtistsList.js
@@ -1,67 +1,87 @@
 const ArtistsList = [
-    {
-        name: 'Skinny Living',
-        type: 'Band',
-        img: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1656595318/Bedmar%20Music/mobile/SkinnyLiving_y7sdbz.png',
-        imgLg: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1656595669/Bedmar%20Music/desktop/SkinnyLiving_rs2wwv.png',
-        desc: 'This is a description of the artist that is pictured behind this useless text',
-        imgZoom: '80%',
-        url: 'https://skinnyliving.co.uk'
-    },
-    // {
-    //     name: 'David King',
-    //     type: 'Producer / Songwriter',
-    //     img: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1656595318/Bedmar%20Music/mobile/DavidKing_yzjzqc.png',
-    //     imgLg: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1656595669/Bedmar%20Music/desktop/DavidKing_srme4k.png',
-    //     desc: 'This is a description of the artist that is pictured behind this useless text',
-    //     imgZoom: '110%',
-    //     url: 'https://www.instagram.com/davidluciusking/ '
-    // },
-    {
-        name: 'Michael Blackwell',
-        type: 'Singer / Songwriter',
-        img: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1678280076/Bedmar%20Music/Michael_Blackwell_2748_ojidrq.png',
-        imgLg: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1678280076/Bedmar%20Music/Michael_Blackwell_2748_ojidrq.png',
-        desc: 'This is a description of the artist that is pictured behind this useless text',
-        imgZoom: '110%',
-        url: 'https://michaelblackwell.co.uk/'
-    },
-    {
-        name: 'Sonni Mills',
-        type: 'Singer / Songwriter',
-        img: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1697725120/Bedmar%20Music/SonniMills_j6ockg.png',
-        imgLg: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1697725120/Bedmar%20Music/SonniMills_j6ockg.png',
-        desc: 'This is a description of the artist that is pictured behind this useless text',
-        imgZoom: '100%',
-        url: 'https://www.instagram.com/sonnimills/'
-    },
-    {
-        name: 'Angus Parkin',
-        type: 'Producer / Songwriter / Musical Director',
-        img: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1670329739/Bedmar%20Music/AngusParkin_mrzqik.png',
-        imgLg: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1670329739/Bedmar%20Music/AngusParkin_mrzqik.png',
-        desc: 'This is a description of the artist that is pictured behind this useless text',
-        imgZoom: '100%',
-        url: 'https://www.angusparkinmusic.com/'
-    },
-    // {
-    //     name: 'Bonze',
-    //     type: 'Artist',
-    //     img: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1697725119/Bedmar%20Music/Bonze_dor298.png',
-    //     imgLg: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1697725119/Bedmar%20Music/Bonze_dor298.png',
-    //     desc: 'This is a description of the artist that is pictured behind this useless text',
-    //     imgZoom: '100%',
-    //     url: 'https://spotify.link/Rj8Q4RdF1Db'
-    // },
-    // {
-    //     name: 'Ryan Johnston',
-    //     type: 'Singer / Songwriter',
-    //     img: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1656595318/Bedmar%20Music/mobile/6_r6kaew.png',
-    //     imgLg: 'https://res.cloudinary.com/dtnif6mzm/image/upload/v1656595669/Bedmar%20Music/desktop/RyanJohnston_qp20mc.png',
-    //     desc: 'This is a description of the artist that is pictured behind this useless text',
-    //     imgZoom: '100%',
-    //     url: 'https://www.instagram.com/__ryanjohnston__/'
-    // },
-]
+	{
+		name: "Skinny Living",
+		type: "Band",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/v1749987965/Bedmar%20Music/SkinnyLiving_crop_elsmve.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1749987624/Bedmar%20Music/SkinnyLiving_x1d37p.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "80%",
+		url: "https://skinnyliving.co.uk",
+	},
 
-export default ArtistsList
+	{
+		name: "Michael Blackwell",
+		type: "Singer / Songwriter",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/c_crop,h_650,g_auto/v1749987641/Bedmar%20Music/MichaelBlackwell_awlxkh.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1749987641/Bedmar%20Music/MichaelBlackwell_awlxkh.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "110%",
+		url: "https://michaelblackwell.co.uk/",
+	},
+
+	{
+		name: "Angus Parkin",
+		type: "Producer / Songwriter / Musical Director",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/c_crop,w_300,h_250,g_auto/v1670329739/Bedmar%20Music/AngusParkin_mrzqik.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1670329739/Bedmar%20Music/AngusParkin_mrzqik.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "100%",
+		url: "https://www.angusparkinmusic.com/",
+	},
+
+	{
+		name: "Hugo Barriol",
+		type: "Singer / Songwriter",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/c_crop,w_750,h_600,g_auto/v1749986061/Bedmar%20Music/HugoBarriol_sohk0z.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1749986061/Bedmar%20Music/HugoBarriol_sohk0z.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "100%",
+		url: "https://hugobarriol.bedmarmusic.com/epk",
+	},
+	{
+		name: "Rachel Newnham",
+		type: "Singer / Songwriter",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/v1749986060/Bedmar%20Music/RachelNewnham_gvafkl.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1749986060/Bedmar%20Music/RachelNewnham_gvafkl.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "100%",
+		url: "https://artists.bedmarmusic.com/rachelnewnham-epk",
+	},
+	{
+		name: "Niamh Evans",
+		type: "Singer / Songwriter",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/c_crop,h_550,g_auto/v1749986058/Bedmar%20Music/NiamhEvans_kzx2ar.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1749986058/Bedmar%20Music/NiamhEvans_kzx2ar.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "100%",
+		url: "https://niamhevans.co.uk/epk",
+	},
+	{
+		name: "Eloise Viola",
+		type: "Singer / Songwriter",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/c_crop,h_700,g_auto/v1749986055/Bedmar%20Music/EloiseViola_ceo3eg.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1749986055/Bedmar%20Music/EloiseViola_ceo3eg.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "100%",
+		url: "https://www.instagram.com/eloiseviola/",
+	},
+	{
+		name: "Holly Holden",
+		type: "Singer / Songwriter",
+		img: "https://res.cloudinary.com/dtnif6mzm/image/upload/c_crop,h_500,g_auto/v1749986052/Bedmar%20Music/HollyHolden_sqj0dg.png",
+		imgLg:
+			"https://res.cloudinary.com/dtnif6mzm/image/upload/v1749986052/Bedmar%20Music/HollyHolden_sqj0dg.png",
+		desc: "This is a description of the artist that is pictured behind this useless text",
+		imgZoom: "100%",
+		url: "https://www.instagram.com/holly_holden/",
+	},
+];
+
+export default ArtistsList;
